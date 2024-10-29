b6c5d66dcb33b57d168103a020f7b3b1f73c2dd9
colef80 <cole_friedman1@baylor.edu>
2024-10-29 15:32:16 -0500

    EC

--- Test_Scores_GUI.py
+++ Test_Scores_GUI.py
@@ -7,7 +7,7 @@ class TestScoresGUI:
         self.main_window = tk.Tk()
 
         #set size of window, confirgure main window by pixels (width x height)
-        self.main_window.geometry('400x200')
+        self.main_window.geometry('600x300')
         self.main_window.title('Student Test Average')
         self.main_window.configure(bg='green')
 
@@ -21,14 +21,14 @@ class TestScoresGUI:
 
 
         #Labels for top frame
-        self.test1 = tk.Label(self.score1_frame, text='Enter the score for test 1: ', bg='green')
-        self.test2 = tk.Label(self.score2_frame, text='Enter the score for test 2: ', bg='green')
-        self.test3 = tk.Label(self.score3_frame, text='Enter the score for test 3: ', bg='green')
+        self.test1 = tk.Label(self.score1_frame, text='Enter the score for test 1:', bg='green', fg='yellow', font=('Comic Sans MS', 20))
+        self.test2 = tk.Label(self.score2_frame, text='Enter the score for test 2:', bg='green', fg='yellow', font=('Comic Sans MS', 20))
+        self.test3 = tk.Label(self.score3_frame, text='Enter the score for test 3:', bg='green', fg='yellow', font=('Comic Sans MS', 20))
 
         #Entry for top frame
-        self.score1_entry = tk.Entry(self.score1_frame, width=6, bg='green')
-        self.score2_entry = tk.Entry(self.score2_frame, width=6, bg='green')
-        self.score3_entry = tk.Entry(self.score3_frame, width=6, bg='green')
+        self.score1_entry = tk.Entry(self.score1_frame, width=5, bg='green', fg='yellow', font=('Comic Sans MS', 20))
+        self.score2_entry = tk.Entry(self.score2_frame, width=5, bg='green', fg='yellow', font=('Comic Sans MS', 20))
+        self.score3_entry = tk.Entry(self.score3_frame, width=5, bg='green', fg='yellow', font=('Comic Sans MS', 20))
 
         self.test1.pack(side='left')
         self.score1_entry.pack(side='left')
@@ -39,19 +39,18 @@ class TestScoresGUI:
 
 
         #Create the widgets for the mid frame 
-        self.average_label = tk.Label(self.mid_frame, text='Average: ', bg='green')
+        self.average_label = tk.Label(self.mid_frame, text='Average:', bg='green', fg='yellow', font=('Comic Sans MS', 20))
 
         self.average_var = tk.StringVar()
 
-        self.average = tk.Label(self.mid_frame, textvariable=self.average_var, bg='green')
+        self.average = tk.Label(self.mid_frame, textvariable=self.average_var, bg='green', fg='yellow', font=('Comic Sans MS', 20))
 
-        self.average_label.pack()
-        self.average.pack()
+        self.average_label.pack(side='left')
+        self.average.pack(side='left')
 
 
-        self.calc_button = tk.Button(self.bottom_frame, text='Average', fg='green', command=self.calc_average)
-        self.calc_button.configure(bg='green')
-        self.quit_button = tk.Button(self.bottom_frame, text='Quit', command=self.main_window.destroy)
+        self.calc_button = tk.Button(self.bottom_frame, text='Average', bg='green', fg='yellow', font=('Comic Sans MS', 20), command=self.calc_average)
+        self.quit_button = tk.Button(self.bottom_frame, text='Quit', bg='green', fg='yellow', font=('Comic Sans MS', 20), command=self.main_window.destroy)
 
 
         self.calc_button.pack(side='left')
@@ -68,7 +67,9 @@ class TestScoresGUI:
         tk.mainloop()
 
     def calc_average(self):
-        pass
+        total = int(self.score1_entry.get()) + int(self.score2_entry.get()) + int(self.score3_entry.get())
+        average = round(total / 3,2)
+        self.average_var.set(average)
 
 #create an instance of our class to run it
 my_gui = TestScoresGUI()
